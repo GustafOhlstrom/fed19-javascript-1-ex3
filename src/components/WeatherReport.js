19076c0ed5088a2b32dbf9b9e490ce3857551bf7
Gustaf Ohlström <gustaf_ohlstrom@hotmail.com>
2020-03-02 10:50:31 +0100

    Removed some spacing

--- src/components/WeatherReport.js
+++ src/components/WeatherReport.js
@@ -7,7 +7,7 @@ const WeatherReport = ({report}) => {
 		<div id="WeatherReport">
 			<div className="card bg-dark text-white">
 				<div className="card-body text-center">
-					<h5 className="card-title mb-4">The temperature in {city} is {temp} &deg; C right now, with a humidity of {humidity} %.</h5>
+					<h5 className="card-title mb-4">The temperature in {city} is {temp}&deg; C right now, with a humidity of {humidity}%.</h5>
 					<h6 className="card-title mb-1">Current weather is:</h6>
 					<WeatherConditions weatherConditions={weatherConditions} />
 				</div>
